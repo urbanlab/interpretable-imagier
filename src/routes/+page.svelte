5331d6a842e9e99a6de2330ea83a159b89132f6a
yassin@siouda.com <yassin@siouda.com>
2023-10-30 15:52:14 +0100

    feat: checkboxes filtering displayed cat & items

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,26 +17,28 @@
 
 	async function parse() {
 		const mainCatLines = await parseCSV('/main.csv');
-		console.log(typeof mainCatLines)
+		console.log(typeof mainCatLines);
 		const catLines = await parseCSV('/category.csv');
 		const itemLines = await parseCSV('/items.csv');
 
 		let categories = catLines.map(([category_label, category_index]) => ({
-				category_index: category_index.replace('\r', ''), // Remove the carriage return "\r
-				category_label,
-				items: itemLines
-					.filter(([pictogram_label, pictogram_file, pictogram_category]) =>
-						pictogram_category?.split(';').find((cat) => cat === category_index.replace('\r', ''))
-					)
-					.map(([pictogram_label, pictogram_file, pictogram_category]) => ({
-						pictogram_file,
-						pictogram_label
-					}))
+			category_index: category_index.replace('\r', ''), // Remove the carriage return "\r
+			category_label,
+			checked: true,
+			items: itemLines
+				.filter(([pictogram_label, pictogram_file, pictogram_category]) =>
+					pictogram_category?.split(';').find((cat) => cat === category_index.replace('\r', ''))
+				)
+				.map(([pictogram_label, pictogram_file, pictogram_category]) => ({
+					pictogram_file,
+					pictogram_label
+				}))
 		}));
 		// if categorie is in mainCatLines.category_index then add mainCatLines.category_label to mainCategories
 		mainCategories = mainCatLines.map(([category_label, category_index]) => ({
 			category_index: category_index.replace('\r', ''), // Remove the carriage return "\r
 			category_label: category_label,
+			checked: true,
 			sub_categorie_items: category_index
 				.split(';')
 				.map((index) => categories.find((cat) => cat.category_index === index))
@@ -46,24 +48,11 @@
 		display = true;
 	}
 
-	function filterCategories(category_index, checked) {
-		if (checked) {
-			selectedCategories.push(category_index);
-		} else {
-			// Only remove the category if it is present
-			const index = selectedCategories.indexOf(category_index);
-			if (index > -1) {
-				selectedCategories.splice(index, 1);
-			}
-		}
-		displayedCategories = categories.filter((cat) =>
-			selectedCategories.includes(cat.category_index)
-		);
-	}
-
-	function removeItem(categoriesIndex,categoryIndex, itemIndex) {
+	function removeItem(categoriesIndex, categoryIndex, itemIndex) {
 		console.log('removeItem', categoryIndex, itemIndex);
-		console.log(displayedCategories[categoriesIndex].sub_categorie_items[categoryIndex].items[itemIndex]);
+		console.log(
+			displayedCategories[categoriesIndex].sub_categorie_items[categoryIndex].items[itemIndex]
+		);
 		const spliced = displayedCategories.filter((cat, catIndex) => {
 			if (catIndex === categoriesIndex) {
 				cat.sub_categorie_items[categoryIndex].items.splice(itemIndex, 1);
@@ -85,6 +74,7 @@
 			><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 			<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 			<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+			<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 			<style>
 				svg {
 					fill: #ffffff;
@@ -98,20 +88,28 @@
 	<div class="print:hidden">
 		<h1>Categories</h1>
 		{#if display}
-			{#each mainCategories as category}
+			{#each mainCategories as category, categoryIndex}
 				<div class="flex flex-col">
 					<div>
 						<input
-						type="checkbox"
-						id={category.category_index}
-						name={category.category_index}
-						value={category.category_index}
-						on:change={(e) => filterCategories(category.category_index, e.target.checked)}
+							type="checkbox"
+							id={category.category_index}
+							name={category.category_index}
+							value={category.category_index}
+							checked={category.checked}
+							on:change={(e) =>
+								displayedCategories = mainCategories.map((cat, catIndex) => {
+									if (catIndex === categoryIndex) {
+										cat.checked = e.target.checked;
+									}
+									return cat;
+								}
+							)}
 						/>
 						<label for={category.category_index} class="ml-2">{category.category_label}</label>
 					</div>
 
-					{#each category.sub_categorie_items as subCategory}
+					{#each category.sub_categorie_items as subCategory, subCategoryIndex}
 						{#if subCategory}
 							<div class="ml-4">
 								<input
@@ -119,12 +117,22 @@
 									id={subCategory.category_index}
 									name={subCategory.category_index}
 									value={subCategory.category_index}
-									on:change={(e) => filterCategories(subCategory.category_index, e.target.checked)}
+									checked={subCategory.checked}
+									on:change={(e) =>
+										displayedCategories = mainCategories.map((cat, catIndex) => {
+											if (catIndex === categoryIndex) {
+												cat.sub_categorie_items[subCategoryIndex].checked = e.target.checked;
+											}
+											return cat;
+										}
+									)}
 								/>
-								<label for={subCategory.category_index} class="ml-2">{subCategory.category_label}</label>
+								<label for={subCategory.category_index} class="ml-2"
+									>{subCategory.category_label}</label
+								>
 							</div>
 						{/if}
-					{/each }
+					{/each}
 				</div>
 			{/each}
 		{/if}
@@ -132,37 +140,42 @@
 	<div>
 		{#if display}
 			{#each displayedCategories as categories, categoriesIndex}
-				<h1 class="text-3xl">{categories?.category_label}</h1>
-				{#each categories.sub_categorie_items as category, categoryIndex}
-					<h1 class="text-xl">{category?.category_label}</h1>
-					<ul class="flex flex-wrap justify-center">
-						{#if category?.items}
-							{#each category.items as item, itemIndex}
-								<li class="w-1/5 border border-solid border-r-4 rounded-lg m-1">
-									<button
-										class="print:hidden absolute -m-2 hover:scale-125"
-										on:click={() => removeItem(categoriesIndex, categoryIndex, itemIndex)}
-									>
-										<svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512"
-											><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-											<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-											<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-											<style>
-												svg {
-													fill: #ff0000;
-												}
-											</style><path
-												d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
-											/></svg
-										>
-									</button>
-									<img src={'/svg/' + item.pictogram_file + '.svg'} alt={item.pictogram_label} />
-									<p>{item.pictogram_label}</p>
-								</li>
-							{/each}
+				{#if categories.checked}
+					<h1 class="text-3xl">{categories?.category_label}</h1>
+					{#each categories.sub_categorie_items as category, categoryIndex}
+						{#if category?.checked}
+							<h1 class="text-xl">{category?.category_label}</h1>
+							<ul class="flex flex-wrap justify-center">
+								{#if category?.items}
+									{#each category.items as item, itemIndex}
+										<li class="w-1/5 border border-solid border-r-4 rounded-lg m-1">
+											<button
+												class="print:hidden absolute -m-2 hover:scale-125"
+												on:click={() => removeItem(categoriesIndex, categoryIndex, itemIndex)}
+											>
+												<svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512"
+													><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+													<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+													<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+													<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+													<style>
+														svg {
+															fill: #ff0000;
+														}
+													</style><path
+														d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"
+													/></svg
+												>
+											</button>
+											<img src={'/svg/' + item.pictogram_file + '.svg'} alt={item.pictogram_label} />
+											<p>{item.pictogram_label}</p>
+										</li>
+									{/each}
+								{/if}
+							</ul>
 						{/if}
-					</ul>
-				{/each}
+					{/each}
+				{/if}
 			{/each}
 		{/if}
 	</div>
